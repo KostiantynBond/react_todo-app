ed8df19a5b3fdbab880dbd89f65716f6a279fe93
Костянтин <91181748+KostiantynBond@users.noreply.github.com>
2025-01-31 01:14:22 +0200

    Delete react_todo-app directory

--- react_todo-app/src/types/ErrorType.ts
+++
@@ -1,8 +0,0 @@
-export enum ErrorType {
-  Default = '',
-  EmptyTitle = 'Title should not be empty',
-  AddTodoFailed = 'Unable to add a todo',
-  DeleteTodoFailed = 'Unable to delete a todo',
-  UpdateTodoFailed = 'Unable to update a todo',
-  LoadTodosFailed = 'Unable to load todos',
-}
